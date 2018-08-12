07678bda0b598f2cbcf5a48357eb8aff43fafffd
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-12 15:00:42 +0100

    Added service worker

--- js/sw/sw.js
+++ js/sw/sw.js
@@ -1,27 +1,38 @@
 /*jshint esversion: 6 */
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('restaurant-static-v1').then((cache) => {
+// Great help from https://developers.google.com/web/fundamentals/codelabs/offline/
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('restaurant-static-v1').then(function(cache) {
       return cache.addAll([
         '/',
-        './restaurant.html',
-        './js/dbhelper.js',
-        './js/main.js',
-        './js/restaurant_info.js',
-        './css/styles.css',
-        './data/restaurants.json',
-        './img/1.jpg',
-        './img/2.jpg',
-        './img/3.jpg',
-        './img/4.jpg',
-        './img/5.jpg',
-        './img/6.jpg',
-        './img/7.jpg',
-        './img/8.jpg',
-        './img/9.jpg',
-        './img/10.jpg'
+        '/index.html',
+        '/restaurant.html',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/css/styles.css',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
       ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
